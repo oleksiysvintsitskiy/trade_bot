c93f5b4c91b1dff9752038d167af2eb1d22ec0e9
src-pwd <dallasopen@hotmail.com>
2018-01-31 04:28:03 +0200

    Update bot.py

--- sz_aloner/bot/bot.py
+++ sz_aloner/bot/bot.py
@@ -4,14 +4,10 @@ import sys
 import random
 
 bot = telebot.TeleBot(config.token)
-bot.send_message(365111522, "Invalid python library name, dolboeb")
+bot.send_message(365111013, "Invalid python library name")
 
 sys.exit()
-#bot.send_message(96580305, "Один. Один. Проверка. Хуй саси губой тряси")
+
 def repeat_all_messages(message):
 	print(message)
 	bot.send_message(chaId, message.text)
-
-# if __name__ == '__main__':
-#     #bot.polling(none_stop=True)
-#     bot.polling()
